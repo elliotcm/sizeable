b8733e89d7f0e8af86c46d79c61c07147182d42f
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2012-01-06 16:05:43 +0000

    Breathing room and whitespace.

--- lib/sizeable/app.rb
+++ lib/sizeable/app.rb
@@ -5,25 +5,28 @@ require 'lib/sizeable/resizer'
 
 module Sizeable
   class App
+
     def call(env)
       begin
         resizer = Resizer.new(Rack::Request.new(env))
         resizer.resize!
-        
+
         headers = {
           "Content-Type" => resizer.content_type,
           'Cache-Control' => "public, max-age=#{days_to_seconds(14)}"
         }
-        
+
         Rack::Response.new(resizer.image_blob, 200, headers).finish
       rescue NoSuchImageException => e
         Rack::Response.new('The requested image was not found.', 404).finish
       end
     end
-    
-    private
+
+  protected
+
     def days_to_seconds(days)
       days * 24 * 60 * 60
     end
+
   end
 end
