9d22868e7dc928ab5c7dccfa6fc875b8d9d333dc
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2012-01-06 16:57:01 +0000

    Put bucket name and dimensions in URL.
    Retain pre-setting of bucket via ENV variable.

--- lib/sizeable/app.rb
+++ lib/sizeable/app.rb
@@ -8,7 +8,7 @@ module Sizeable
 
     def call(env)
       begin
-        resizer = Resizer.new(Rack::Request.new(env))
+        resizer = Resizer.new(*parse_request(env))
         resizer.resize!
 
         headers = {
@@ -17,7 +17,7 @@ module Sizeable
         }
 
         Rack::Response.new(resizer.image_blob, 200, headers).finish
-      rescue NoSuchImageException => e
+      rescue NoSuchImageException
         Rack::Response.new('The requested image was not found.', 404).finish
       end
     end
@@ -26,6 +26,29 @@ module Sizeable
 
     def days_to_seconds(days)
       days * 24 * 60 * 60
+    end
+
+    def parse_request(env)
+      request = Rack::Request.new(env)
+
+      parts = request.path_info.split('/').reject{|str| str.nil? or str == ''}
+
+      raise NoSuchImageException if parts.length < 2
+
+      s3_bucket = preset_s3_bucket || parts.shift
+
+      if parts[-2] =~ /^(\d+)x(\d+)$/
+        parts.delete_at(-2)
+        width, height = $1, $2
+      end
+
+      image_name = parts.join('/')
+
+      return [s3_bucket, image_name, width, height]
+    end
+
+    def preset_s3_bucket
+      ENV['S3_BUCKET']
     end
 
   end
--- lib/sizeable/resizer.rb
+++ lib/sizeable/resizer.rb
@@ -3,29 +3,27 @@ require 'RMagick'
 
 module Sizeable
   class Resizer
-    def initialize(request)
-      raise NoSuchImageException.new if image_name(request.path_info) == ''
-
+    def initialize(s3_bucket, image_name, width, height)
       connect_to_s3
       begin
-        s3_object = AWS::S3::S3Object.find(image_name(request.path_info), s3_bucket)
+        s3_object = AWS::S3::S3Object.find(image_name, s3_bucket)
 
         @image_blob = s3_object.value
         @image = Magick::Image.from_blob(@image_blob).first
 
         @content_type = s3_object.content_type
-      rescue AWS::S3::NoSuchKey => e
-        raise NoSuchImageException.new
+      rescue AWS::S3::NoSuchBucket, AWS::S3::NoSuchKey
+        raise NoSuchImageException
       end
 
-      @width = request.params['width'].to_i
-      @height = request.params['height'].to_i
+      @width = width
+      @height = height
     end
 
     attr_reader :image_blob, :content_type
 
     def resize!
-      return if missing_boundary
+      return if missing_boundary?
       @image = @image.resize_to_fit(@width, @height)
       @image_blob = @image.to_blob
     end
@@ -38,15 +36,7 @@ module Sizeable
       )
     end
 
-    def s3_bucket
-      ENV['S3_BUCKET']
-    end
-
-    def image_name(path)
-      path.slice(1, path.length)
-    end
-
-    def missing_boundary
+    def missing_boundary?
       @width.nil? or @height.nil? or @width == 0 or @height == 0
     end
   end
